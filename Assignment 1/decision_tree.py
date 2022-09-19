af95754c14a6d2cf9b96580cf9622f61d4805be1
RaheelSyedAhmed <45162206+RaheelSyedAhmed@users.noreply.github.com>
2022-09-19 01:31:28 -0500

    Add files via upload

---
+++ Assignment 1/decision_tree.py
@@ -0,0 +1,250 @@
+# decision_tree.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# personal and educational purposes provided that (1) you do not distribute
+# or publish solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UT Dallas, including a link to http://cs.utdallas.edu.
+#
+# This file is part of Programming Assignment 1 for CS6375: Machine Learning.
+# Gautam Kunapuli (gautam.kunapuli@utdallas.edu)
+# Sriraam Natarajan (sriraam.natarajan@utdallas.edu),
+#
+#
+# INSTRUCTIONS:
+# ------------
+# 1. This file contains a skeleton for implementing the ID3 algorithm for
+# Decision Trees. Insert your code into the various functions that have the
+# comment "INSERT YOUR CODE HERE".
+#
+# 2. Do NOT modify the classes or functions that have the comment "DO NOT
+# MODIFY THIS FUNCTION".
+#
+# 3. Do not modify the function headers for ANY of the functions.
+#
+# 4. You may add any other helper functions you feel you may need to print,
+# visualize, test, or save the data and results. However, you MAY NOT utilize
+# the package scikit-learn OR ANY OTHER machine learning package in THIS file.
+
+import numpy as np
+
+
+def partition(x):
+    """
+    Partition the column vector x into subsets indexed by its unique values (v1, ... vk)
+
+    Returns a dictionary of the form
+    { v1: indices of x == v1,
+      v2: indices of x == v2,
+      ...
+      vk: indices of x == vk }, where [v1, ... vk] are all the unique values in the vector z.
+    """
+
+    final_dict = dict()
+    unique_vals = np.unique(x)
+    for value in unique_vals:
+        final_dict[value] = np.array(np.where(x == value))
+    return final_dict
+    #raise Exception('Function not yet implemented!')
+
+
+def entropy(y):
+    """
+    Compute the entropy of a vector y by considering the counts of the unique values (v1, ... vk), in z
+
+    Returns the entropy of z: H(z) = p(z=v1) log2(p(z=v1)) + ... + p(z=vk) log2(p(z=vk))
+    """
+
+    # INSERT YOUR CODE HERE
+    final_dict = dict()
+    unique_vals = np.unique(y)
+    tot_count = 0
+    for value in unique_vals:
+        count = np.array(np.where(y == value)).size
+        tot_count += count
+        final_dict[value] = count
+    entropy = 0
+    for key in final_dict.keys():
+        p_z = final_dict[key] / tot_count
+        entropy_component = p_z * np.log2(p_z)
+        entropy += entropy_component
+    
+    return entropy
+
+    #raise Exception('Function not yet implemented!')
+
+
+def mutual_information(x, y):
+    """
+    Compute the mutual information between a data column (x) and the labels (y). The data column is a single attribute
+    over all the examples (n x 1). Mutual information is the difference between the entropy BEFORE the split set, and
+    the weighted-average entropy of EACH possible split.
+
+    Returns the mutual information: I(x, y) = H(y) - H(y | x)
+    """
+
+    # INSERT YOUR CODE HERE
+    h_y = entropy(y)
+    x_part = partition(x)   # Find all unique values and their indices in X.
+
+    tot_size = y.size           # Total number of rows.
+    weighted_average_sum = 0
+    for key in x_part.keys():
+        attr_val_list = x_part[key][0]      # Found all the indices to examine
+        attr_val_size = attr_val_list.size  # Found number of indices to represent weight (when divided by the total)
+        entropy_part_y = entropy(y[attr_val_list])   # Calculate the entropy of the y column where X = x_val to see entropy of that split.
+        weight = attr_val_size / tot_size
+        weighted_average_sum += (weight * entropy_part_y)
+    
+    return (h_y - weighted_average_sum)   # Difference in entropy before and average entropy after.
+    
+    #raise Exception('Function not yet implemented!')
+
+
+def id3(x, y, attribute_value_pairs=None, depth=0, max_depth=5):
+    """
+    Implements the classical ID3 algorithm given training data (x), training labels (y) and an array of
+    attribute-value pairs to consider. This is a recursive algorithm that depends on three termination conditions
+        1. If the entire set of labels (y) is pure (all y = only 0 or only 1), then return that label
+        2. If the set of attribute-value pairs is empty (there is nothing to split on), then return the most common
+           value of y (majority label)
+        3. If the max_depth is reached (pre-pruning bias), then return the most common value of y (majority label)
+    Otherwise the algorithm selects the next best attribute-value pair using INFORMATION GAIN as the splitting criterion
+    and partitions the data set based on the values of that attribute before the next recursive call to ID3.
+
+    The tree we learn is a BINARY tree, which means that every node has only two branches. The splitting criterion has
+    to be chosen from among all possible attribute-value pairs. That is, for a problem with two features/attributes x1
+    (taking values a, b, c) and x2 (taking values d, e), the initial attribute value pair list is a list of all pairs of
+    attributes with their corresponding values:
+    [(x1, a),
+     (x1, b),
+     (x1, c),
+     (x2, d),
+     (x2, e)]
+     If we select (x2, d) as the best attribute-value pair, then the new decision node becomes: [ (x2 == d)? ] and
+     the attribute-value pair (x2, d) is removed from the list of attribute_value_pairs.
+
+    The tree is stored as a nested dictionary, where each entry is of the form
+                    (attribute_index, attribute_value, True/False): subtree
+    * The (attribute_index, attribute_value) determines the splitting criterion of the current node. For example, (4, 2)
+    indicates that we test if (x4 == 2) at the current node.
+    * The subtree itself can be nested dictionary, or a single label (leaf node).
+    * Leaf nodes are (majority) class labels
+
+    Returns a decision tree represented as a nested dictionary, for example
+    {(4, 1, False):
+        {(0, 1, False):
+            {(1, 1, False): 1,
+             (1, 1, True): 0},
+         (0, 1, True):
+            {(1, 1, False): 0,
+             (1, 1, True): 1}},
+     (4, 1, True): 1}
+    """
+    
+    # INSERT YOUR CODE HERE. NOTE: THIS IS A RECURSIVE FUNCTION.
+    
+    # Attribute values setter
+    
+
+    # Feature selection
+    feature_mutual_infos = np.array([])
+    for i in range(np.shape(x)[1]):
+        feature_mutual_infos = np.append(feature_mutual_infos, mutual_information(x[:, i], y))
+    feature_selected = (np.argmin(feature_mutual_infos))
+
+    # Termination condition 1
+    if np.all(y == y[0]):
+        return y[0]
+    # Termination condition 2
+    if not attribute_value_pairs:
+        return np.bincount(y).argmax()
+    # Termination condition 3
+    if depth == max_depth:
+        return np.bincount(y).argmax()
+    
+    
+    
+
+    raise Exception('Function not yet implemented!')
+
+
+def predict_example(x, tree):
+    """
+    Predicts the classification label for a single example x using tree by recursively descending the tree until
+    a label/leaf node is reached.
+
+    Returns the predicted label of x according to tree
+    """
+
+    # INSERT YOUR CODE HERE. NOTE: THIS IS A RECURSIVE FUNCTION.
+    raise Exception('Function not yet implemented!')
+
+
+def compute_error(y_true, y_pred):
+    """
+    Computes the average error between the true labels (y_true) and the predicted labels (y_pred)
+
+    Returns the error = (1/n) * sum(y_true != y_pred)
+    """
+
+    # INSERT YOUR CODE HERE
+    raise Exception('Function not yet implemented!')
+
+
+def visualize(tree, depth=0):
+    """
+    Pretty prints (kinda ugly, but hey, it's better than nothing) the decision tree to the console. Use print(tree) to
+    print the raw nested dictionary representation.
+    DO NOT MODIFY THIS FUNCTION!
+    """
+
+    if depth == 0:
+        print('TREE')
+
+    for index, split_criterion in enumerate(tree):
+        sub_trees = tree[split_criterion]
+
+        # Print the current node: split criterion
+        print('|\t' * depth, end='')
+        print('+-- [SPLIT: x{0} = {1}]'.format(split_criterion[0], split_criterion[1]))
+
+        # Print the children
+        if type(sub_trees) is dict:
+            visualize(sub_trees, depth + 1)
+        else:
+            print('|\t' * (depth + 1), end='')
+            print('+-- [LABEL = {0}]'.format(sub_trees))
+
+
+if __name__ == '__main__':
+    
+    # Load the training data
+    M = np.genfromtxt('./monks-1.train', missing_values=0, skip_header=0, delimiter=',', dtype=int)
+    ytrn = M[:, 0]
+    Xtrn = M[:, 1:]
+    #print(partition(ytrn))
+    #print(entropy(np.array([1,1,1,3,2,1])))
+    #mutual_information(Xtrn[:, 0], ytrn)
+    #mutual_information(Xtrn[:, 1], ytrn)
+    #mutual_information(Xtrn[:, 2], ytrn)
+    #mutual_information(Xtrn[:, 3], ytrn)
+    #mutual_information(Xtrn[:, 4], ytrn)
+    #mutual_information(Xtrn[:, 5], ytrn)
+    id3(Xtrn, ytrn, max_depth=3)
+    """
+    # Load the test data
+    M = np.genfromtxt('./monks-1.test', missing_values=0, skip_header=0, delimiter=',', dtype=int)
+    ytst = M[:, 0]
+    Xtst = M[:, 1:]
+
+    # Learn a decision tree of depth 3
+    decision_tree = id3(Xtrn, ytrn, max_depth=3)
+    visualize(decision_tree)
+
+    # Compute the test error
+    y_pred = [predict_example(x, decision_tree) for x in Xtst]
+    tst_err = compute_error(ytst, y_pred)
+
+    print('Test Error = {0:4.2f}%.'.format(tst_err * 100))
+    """
+    #print(entropy([1,1,1,3,2,1]))
